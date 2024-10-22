2d902851dbe06d41b95c2a75a5b75c1ffcb3b733
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-10-22 21:33:57 +0900

    Title: 다양하게 출력, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241022/다양하게 출력/print-in-variety.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Total days in Year");// 여기에 코드를 작성해주세요.
+        System.out.println(365);
+        System.out.println("Circumference rate");
+        System.out.println(3.1415926535);
+    }
+}
