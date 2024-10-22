bdffa6dc7ac6eb96f5c557115e1c4b3b75db77c8
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-10-22 21:43:35 +0900

    Title: 변수 출력하기, Time: 83ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241022/변수 출력하기/outputing-variables.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        String b = "C";
+        System.out.println(a + "..." + b);// 여기에 코드를 작성해주세요.
+    }
+}
