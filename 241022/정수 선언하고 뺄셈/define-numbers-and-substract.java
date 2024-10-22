1cbd7001c5e2f28a34bd0899fbd1768a98c4f46c
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-10-22 21:37:19 +0900

    Title: 정수 선언하고 뺄셈, Time: 82ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241022/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 97;
+        int num2 = 13;
+        int sum = num1 - num2;
+        System.out.println(num1 + " - " + num2 + " = " + sum); // 여기에 코드를 작성해주세요.
+    }
+}
