d9656c1c56d6bfc4f16956850ed9d1efab0e8e97
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-10-23 20:18:58 +0900

    Title: 변수 값 교체하기 6, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241023/변수 값 교체하기 6/replacing-variable-values-6.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 2; int b = 5;
+        int temp;
+        temp = a;
+        a = b;
+        b = temp;
+         System.out.println( a + "\n" + b);// 여기에 코드를 작성해주세요.
+    }
+}
