6e8238deccc2a8b24e1a8afa7d3071dfb2c201d7
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-10-23 20:57:52 +0900

    Title: 입력받은 값과 합 출력, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241023/입력받은 값과 합 출력/output-of-value-and-sum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(a + " " + b + " " + sum);// 여기에 코드를 작성해주세요.
+    }
+}
