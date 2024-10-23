75174ee5a3f96db6712ef623d4661416cd2dad8d
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-10-23 20:28:42 +0900

    Title: 변수 값 복사하기 2, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241023/변수 값 복사하기 2/copying-variable-values-2.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b= 6;
+        int c =7;
+        a=b=c;
+        System.out.println(a + " " + b +" " + c);// 여기에 코드를 작성해주세요.
+    }
+}
