88c0cfa0a330a11f7a81a8ee1b9f1c004a713c02
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-10-23 20:42:51 +0900

    Title: 실수 받아 그대로 출력, Time: 149ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 241023/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double n = sc.nextDouble();
+        System.out.printf("%.2f", n);// 여기에 코드를 작성해주세요.
+    }
+}
