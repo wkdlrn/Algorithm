2419f42c86078416a97d5a4118776da49bd5a096
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-10-23 20:30:06 +0900

    Title: 합을 복사하기, Time: 83ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241023/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a =1;
+        int b= 2;
+        int c= 3;
+        int sum = a + b + c;
+        a =b= c= sum;
+        System.out.println(a + " " + b + " " + c);// 여기에 코드를 작성해주세요.
+    }
+}
