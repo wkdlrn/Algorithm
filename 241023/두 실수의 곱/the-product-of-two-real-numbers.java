84a3bef1c8c105723514a6e5d6432cb1e506d935
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-10-23 20:08:32 +0900

    Title: 두 실수의 곱, Time: 98ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241023/두 실수의 곱/the-product-of-two-real-numbers.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double a = 5.26;
+        double b = 8.27;
+        double c = a * b;
+        System.out.printf("%.3f", c);// 여기에 코드를 작성해주세요.
+    }
+}
