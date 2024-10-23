008a379204246df71e649a6cc2eb2af5924ae6e0
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-10-23 20:39:16 +0900

    Title: 정수 입력받아 계산, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241023/정수 입력받아 계산/calculation-by-inputting-an-integer.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int sum = a * 2 + 3;
+        System.out.println(sum);// 여기에 코드를 작성해주세요.
+    }
+}
