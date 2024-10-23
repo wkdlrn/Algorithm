f021ff9e32ffd378b815aba5168a7bd0b604d34b
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-10-23 21:03:18 +0900

    Title: 실수 입력받아 계산, Time: 172ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 241023/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);// 여기에 코드를 작성해주세요.
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double sum = a + b;
+        System.out.printf("%.2f", sum);
+    }
+}
