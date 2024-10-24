a90f9e1d45fd2d796294f2ad79f3f7b5a874e248
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-10-24 21:58:10 +0900

    Title: 합과 평균, Time: 185ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241024/합과 평균/sum-and-avg.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        double average = (double) (a + b) / 2;
+        System.out.println(sum + " " + average);// 여기에 코드를 작성해주세요.
+    }
+}
