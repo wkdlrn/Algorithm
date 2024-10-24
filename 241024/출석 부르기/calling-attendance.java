4fbc6b5b8ae3970788f56035a09a7b4b4de68831
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-10-24 22:33:52 +0900

    Title: 출석 부르기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 241024/출석 부르기/calling-attendance.java
+++ 241024/출석 부르기/calling-attendance.java
@@ -11,6 +11,8 @@ public class Main {
             System.out.println("Tom");
         } else if (n == 3) {
             System.out.println("Paul");
+        } else {
+            System.out.println("Vacancy");
         }
     }
 }
