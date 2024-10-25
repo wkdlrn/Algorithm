5cce150e75c55337b50fb514aacbf8ed6bd79dfc
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-10-25 18:17:40 +0900

    Title: 일 수 구하기, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

--- 241025/일 수 구하기/number-of-days-in-month.java
+++ 241025/일 수 구하기/number-of-days-in-month.java
@@ -6,7 +6,7 @@ public class Main {
         int n = sc.nextInt();
         if (n == 2) {
             System.out.println("28");
-        } else if (n == 1 || n == 3 || n ==5 ||n==7||n==8||n==10||n==11){
+        } else if (n == 1 || n == 3 || n ==5 ||n==7||n==8||n==10||n==12){
             System.out.println("31");
         } else {
             System.out.println("30");
