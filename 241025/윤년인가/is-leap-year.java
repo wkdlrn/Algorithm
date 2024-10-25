a9ab61ea983652f59d80614b6f4d3f593ed9782d
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-10-25 18:04:24 +0900

    Title: 윤년인가, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 241025/윤년인가/is-leap-year.java
+++ 241025/윤년인가/is-leap-year.java
@@ -5,12 +5,10 @@ public class Main {
         
         int y = sc.nextInt();
 
-        if (y % 100 == 0 && y % 400 != 0) {
-            System.out.println("false");
-        } else if (y % 4 == 0) {
+        if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0)) {
             System.out.println("true");
         } else {
-            System.out.println("true");
+            System.out.println("false");
         }
     }
 }
