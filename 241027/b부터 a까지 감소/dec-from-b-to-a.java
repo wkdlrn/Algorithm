cb1a198da19aa05338bb9bce5fcaa0e1206ae213
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-10-27 23:55:16 +0900

    Title: b부터 a까지 감소, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 241027/b부터 a까지 감소/dec-from-b-to-a.java
+++ 241027/b부터 a까지 감소/dec-from-b-to-a.java
@@ -6,7 +6,7 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        for(int i = a; i >= b; i--) {
+        for(int i = b; i >= a; i--) {
             System.out.print(i + " ");
         }
     }
