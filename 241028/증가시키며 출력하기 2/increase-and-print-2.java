531df6f64ac668e5074df1851c60e8ee888ea70e
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-10-28 00:22:21 +0900

    Title: 증가시키며 출력하기 2, Time: 79ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241028/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 10;                  // 시작 값(초기화식)
+
+        while (i <= 26) {           // 계속 반복할 조건(조건식)
+            System.out.print(i + " ");  // 원하는 코드 작성
+            i++;                    // 원하는 증감값 설정(증감식)
+        }
+
+    }
+}
