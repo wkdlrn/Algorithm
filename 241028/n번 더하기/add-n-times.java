ff781cf9757f139144baea2544b741c7188ce7ce
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-10-28 22:46:47 +0900

    Title: n번 더하기, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241028/n번 더하기/add-n-times.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int n = sc.nextInt();
+        int sum = a + n;
+
+        for(int i = 0; i < n; i++) {
+            System.out.println(sum);
+            sum += n;
+        }
+    }
+}
