ae052895e0006357538cd7750443e9719313d4ba
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-10-28 22:41:52 +0900

    Title: a/b 출력, Time: 225ms, Memory: 10MB, Status: Passed - CodeTree

--- 241028/a/b 출력/a-divide-b.java
+++ 241028/a/b 출력/a-divide-b.java
@@ -8,15 +8,18 @@ public class Main {
         int a = scanner.nextInt();
         int b = scanner.nextInt();
         
-        // 나눗셈 연산 수행
-        double result = (double) a / b;
+        System.out.print(a / b + ".");
+//a를 b로 나눈 나머지에 10을 곱한 값을 
+//b로 나눴을 때의 몫을 순서대로 적는것
+        a %= b;
+        for(int i = 0; i < 20; i++) {
+            a *= 10;
+            System.out.print(a / b);
 
-        // String.format을 사용해 소수점 20자리까지 문자열로 변환
-        String formattedResult = String.format("%.20f", result);
+            a %= b;
+        }
+       
 
-        // 21번째 자리에서 내림하기 위해 22번째 문자까지 잘라서 출력
-        System.out.println(formattedResult.substring(0, 22));
-        
-        scanner.close();
+    
     }
 }
