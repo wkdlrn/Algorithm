558ec97f8da878951f3ae4cbcd726f844c50ce53
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-10-28 22:29:42 +0900

    Title: a/b 출력, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241028/a/b 출력/a-divide-b.java
+++ 241028/a/b 출력/a-divide-b.java
@@ -1,10 +1,22 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        Scanner scanner = new Scanner(System.in);
+
+        // 두 정수 a와 b 입력 받기
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        
+        // 나눗셈 연산 수행
         double result = (double) a / b;
-        System.out.printf("%.20f",result);
+
+        // String.format을 사용해 소수점 20자리까지 문자열로 변환
+        String formattedResult = String.format("%.20f", result);
+
+        // 21번째 자리에서 내림하기 위해 22번째 문자까지 잘라서 출력
+        System.out.println(formattedResult.substring(0, 22));
+        
+        scanner.close();
     }
 }
