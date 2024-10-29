397e71e8e1834757713a6fcf86404f802e2656b7
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-10-29 21:34:55 +0900

    Title: 369 게임, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 241029/369 게임/369-game.java
+++ 241029/369 게임/369-game.java
@@ -6,10 +6,15 @@ public class Main {
         int n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 || i % 6 == 0 || i % 9 == 0) {
+            if (i % 3 == 0) {// 3의 배수 
                 System.out.print("0" + " ");
+            } else if (i % 10 == 3 || i % 10 == 6 || i % 10 == 9){
+                //i를 10으로 나눴을 때 나머지 또는 몫 3, 6, 9
+                System.out.print("0 ");
+            } else if (i / 10 == 3 || i / 10 == 6 || i / 10 == 9) {
+                System.out.print("0 ");
             } else {
-                System.out.print(i + " ");
+                System.out.print(i +" ");
             }
         }
     }
