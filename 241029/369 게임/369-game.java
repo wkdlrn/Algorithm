6d889c1d93401792c9434d75e595b6bf3a23079a
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-10-29 21:27:57 +0900

    Title: 369 게임, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241029/369 게임/369-game.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 || i % 6 == 0 || i % 9 == 0) {
+                System.out.print("0" + " ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
