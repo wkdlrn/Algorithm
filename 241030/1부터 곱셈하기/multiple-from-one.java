bf6552b01b457a325e0980d6e7de29399351ad9e
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-10-30 19:26:44 +0900

    Title: 1부터 곱셈하기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241030/1부터 곱셈하기/multiple-from-one.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int sum = 0;
+        int prod = 1;
+        for(int i = 1; i <= 10; i++) {
+            prod *= i;
+            if (prod >= n) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
