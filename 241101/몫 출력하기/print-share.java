c6b8a0752fa3eacb1d3769ab130028ed3401a4d8
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-11-01 00:23:24 +0900

    Title: 몫 출력하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241101/몫 출력하기/print-share.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = 0;
+        while(i <= 3) {
+            int n = sc.nextInt();
+            i++;
+            if(n % 2 == 1 ) {
+                continue;
+            } else {
+                System.out.println(n /2);
+            }
+                
+        }
+    }
+}
