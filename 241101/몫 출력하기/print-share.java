c74fa7c880a295839393a6e1f3ac26908b05fcd2
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-11-01 00:27:32 +0900

    Title: 몫 출력하기, Time: 169ms, Memory: 10MB, Status: Passed - CodeTree

--- 241101/몫 출력하기/print-share.java
+++ 241101/몫 출력하기/print-share.java
@@ -3,15 +3,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int i = 0;
-        while(i <= 3) {
+        while(i < 3) {
             int n = sc.nextInt();
-            i++;
+            
             if(n % 2 == 1 ) {
                 continue;
             } else {
+                i++;
                 System.out.println(n /2);
-            }
-                
+            }     
         }
     }
 }
