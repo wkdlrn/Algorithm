aff9c267738fa3652fe490667c6a67cdb7f07882
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-11-05 00:03:09 +0900

    Title: 별표 출력하기, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 241104/별표 출력하기/print-star.java
+++ 241104/별표 출력하기/print-star.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         int n = sc.nextInt();
 
         for(int i = 0; i < n; i++) {
@@ -11,9 +11,9 @@ public class Main {
             }
             System.out.println();
         }
-        
-        for(int i = n - 1; i >= 0; i--) {
-            for(int j = 0; j < i; j++) {
+
+        for(int i = n - 2; i >= 0; i--) {
+            for(int j = 0; j < i + 1; j++) {
                 System.out.print("* ");
             }
             System.out.println();
