2f2e5de3373567cfcfbc41ed3a6de54bad7e9df7
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-11-07 21:11:15 +0900

    Title: 숫자 삼각형, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241107/숫자 삼각형/number-triangle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n ; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
