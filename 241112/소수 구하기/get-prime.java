a7820ad75e6e2766bfb4d829d7ee040b48765829
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-11-12 19:28:54 +0900

    Title: 소수 구하기, Time: 94ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 241112/소수 구하기/get-prime.java
+++ 241112/소수 구하기/get-prime.java
@@ -1,12 +1,5 @@
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int n = sc.nextInt();
-
-        for(int i = 1; i <= n; i++) {
-            if()
-        }
+        // 여기에 코드를 작성해주세요.
     }
 }
