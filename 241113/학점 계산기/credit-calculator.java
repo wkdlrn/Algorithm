890926cc406b9f19ac0df1f315c37110168bd085
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-11-13 23:16:11 +0900

    Title: 학점 계산기, Time: 143ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 241113/학점 계산기/credit-calculator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n  = sc.nextInt();
+        double[] arr = new double[n];
+        double sum = 0;
+        double avg;
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextDouble();
+            sum += arr[i];
+        }
+        avg =  sum / n ;
+        System.out.printf("%.1f \n", avg);
+
+        if (avg >= 4.0) {
+            System.out.println("Perfect");
+        } else if (avg >= 3.0) {
+            System.out.println("Good");
+        } else {
+            System.out.println("Poor");
+        }
+    }
+}
