2db99ee8d0d32ec9d3335a7e78edd7d819314d77
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-11-13 23:31:25 +0900

    Title: 배열 채우기, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241113/배열 채우기/filling-array.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[10];
+        int cnt = 0;
+        for (int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+            if(arr[i] == 0) {
+                break;
+            }
+            cnt++;
+        }
+        for (int i = cnt - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
