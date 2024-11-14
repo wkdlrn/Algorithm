56ee23c8628ec3fb78dbe17d2ff74b696ada8a16
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-11-15 02:32:19 +0900

    Title: 특정 위치의 문자 2, Time: 116ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241115/특정 위치의 문자 2/char-in-specific-location-2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] arr = new String[10];
+    
+        for(int i = 0; i < 10;i++) {
+            arr[i] = sc.next();
+            
+        }
+        for(int i = 1; i < 10;i +=3) {
+            
+             System.out.print(arr[i] + " ");
+            
+            
+        }
+    }
+}
