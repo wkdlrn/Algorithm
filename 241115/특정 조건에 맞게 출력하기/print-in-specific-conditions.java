0ff3a0d6a0c927de1873d8d86c0addfa143fee21
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-11-15 18:30:33 +0900

    Title: 특정 조건에 맞게 출력하기, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241115/특정 조건에 맞게 출력하기/print-in-specific-conditions.java
+++ 241115/특정 조건에 맞게 출력하기/print-in-specific-conditions.java
@@ -3,17 +3,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int [] arr = new int [10];
+        int[] arr = new int[10];
+
+        // 입력값 검증
         for (int i = 0; i < 10; i++) {
-            arr[i] = sc.nextInt();
-        }
-        for (int i = 0; i < 10; i++) {
-            if(arr[i] % 2 == 1) {
-                arr[i] = arr[i] + 3;
+            if (sc.hasNextInt()) {
+                arr[i] = sc.nextInt();
             } else {
-                if (arr[i] == 0) {
-                    break;
-                }
+                System.out.println("10개의 정수를 입력해야 합니다.");
+                return; // 입력 부족 시 프로그램 종료
+            }
+        }
+
+        // 처리 및 출력
+        for (int i = 0; i < 10; i++) {
+            if (arr[i] == 0) { // 0이면 종료
+                break;
+            }
+
+            if (arr[i] % 2 == 1) { // 홀수일 경우
+                arr[i] = arr[i] + 3;
+            } else { // 짝수일 경우
                 arr[i] = arr[i] / 2;
             }
 
