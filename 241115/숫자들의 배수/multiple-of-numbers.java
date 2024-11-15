f6c656a62947c82f2d8009080b4bb35e1fa26125
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-11-15 18:20:19 +0900

    Title: 숫자들의 배수, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 241115/숫자들의 배수/multiple-of-numbers.java
+++ 241115/숫자들의 배수/multiple-of-numbers.java
@@ -10,8 +10,16 @@ public class Main {
         for (int i = 1; i < 10; i++) {
             arr[i] = arr[i-1] + n;
         }
+        int cnt = 0;
         for (int i = 0; i < 10; i++) {
+            if(arr[i] % 5 == 0) {
+                cnt++;
+            }
             System.out.print(arr[i] + " ");
+            if(cnt == 2) {
+                break;
+            }
+
         }
     }
 }
