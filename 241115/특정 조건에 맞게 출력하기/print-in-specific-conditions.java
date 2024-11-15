54d81714071248cf2c420e2e5ef666aa70bdfa33
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-11-15 18:34:13 +0900

    Title: 특정 조건에 맞게 출력하기, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 241115/특정 조건에 맞게 출력하기/print-in-specific-conditions.java
+++ 241115/특정 조건에 맞게 출력하기/print-in-specific-conditions.java
@@ -3,28 +3,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[10];
 
-        // 입력값 검증
-        for (int i = 0; i < 10; i++) {
-            if (sc.hasNextInt()) {
-                arr[i] = sc.nextInt();
-            }
-        }
+        // 배열 선언
+        int[] arr = new int[100];
+        int[] newArr = new int[100];
+        int zeroPoint = 0;
 
-        // 처리 및 출력
-        for (int i = 0; i < 10; i++) {
-            if (arr[i] == 0) { // 0이면 종료
+        // 입력받은 정수로부터 출력할 값을 새로운 배열에 저장하고, 0이 나오면 for문을 탈출
+        for(int i = 0; i < 100; i++) {
+            arr[i] = sc.nextInt();
+            if(arr[i] == 0) {
+                zeroPoint = i;
                 break;
             }
+            if(arr[i] % 2 == 0)
+                newArr[i] = arr[i] / 2;
+            else
+                newArr[i] = arr[i] + 3;
+        }
 
-            if (arr[i] % 2 == 1) { // 홀수일 경우
-                arr[i] = arr[i] + 3;
-            } else { // 짝수일 경우
-                arr[i] = arr[i] / 2;
-            }
-
-            System.out.print(arr[i] + " ");
+        // 0이 나오기 전까지의 배열의 인덱스에 저장된 값을 출력
+        for(int i = 0; i < zeroPoint; i++) {
+            System.out.print(newArr[i] + " ");
         }
     }
 }
