debafb4bbddf8e7061a386693eb741662b66eb49
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-11-15 18:23:38 +0900

    Title: 특정 조건에 맞게 출력하기, Time: 128ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 241115/특정 조건에 맞게 출력하기/print-in-specific-conditions.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        int [] arr = new int [10];
+
+        for (int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < 10; i++) {
+            if(arr[i] % 2 == 1) {
+                arr[i] = arr[i] + 3;
+            } else {
+                arr[i] = arr[i] / 2;
+            }
+            if(arr[i] == 0) {
+                break;
+            }
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
