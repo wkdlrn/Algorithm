aaa9d974748498cb9b856f0759234d995e2138fb
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-11-16 17:59:50 +0900

    Title: 십의 자리 숫자, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241116/십의 자리 숫자/number-of-tens-digit.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[100];
+        int[] newArr = new int[100];
+        int[] countArr = new int[11];
+        int zeropoint = 0;
+
+        for (int i = 0; i < 100; i++) {
+            arr[i] = sc.nextInt();
+            if (arr[i] < 10) {
+                if (arr[i] == 0) {
+                    zeropoint = i;
+                    break;
+                }
+            } else {
+                arr[i] = arr[i] / 10;
+                newArr[i] = arr[i];
+            }
+
+        }
+
+        // 개수 세기
+        for (int i = 0; i < zeropoint; i++) {
+            countArr[newArr[i]]++;
+        }
+
+        // 개수 출력
+        for (int i = 1; i < 10; i++) {
+            System.out.println(i + " - " +countArr[i]);
+        }
+    }
+}
