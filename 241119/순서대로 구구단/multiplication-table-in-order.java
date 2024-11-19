70da85ef3e782f6592c40dae64cbb24df453a36f
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-11-19 14:34:57 +0900

    Title: 순서대로 구구단, Time: 112ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241119/순서대로 구구단/multiplication-table-in-order.java
+++ 241119/순서대로 구구단/multiplication-table-in-order.java
@@ -1,14 +1,33 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
         int a = sc.nextInt();
         int b = sc.nextInt();
-
-        for (int i = 1; i <= 9; i++) {
-            System.out.println(a + " * " + i + " = " + a * i + "  " + b + " * " + i + " = " + b * i);
+        if (a < b) {
+            for (int j = 1; j <= 9; j++) {
+            for (int i = a; i <= b; i++) {
+                System.out.print(i + " * " + j + " = " + (i * j));
+                if (i < b) { // 마지막 단이 아니면 공백 추가
+                    System.out.print("  ");
+                }
+            }
+            System.out.println(); // 한 줄 끝나면 줄바꿈
         }
+        } else {
+             for (int j = 1; j <= 9; j++) {
+            for (int i = b; i >=a; i--) {
+                System.out.print(i + " * " + j + " = " + (i * j));
+                if (i > a) { // 마지막 단이 아니면 공백 추가
+                    System.out.print("  ");
+                }
+            }
+            System.out.println(); // 한 줄 끝나면 줄바꿈
+        }
+        }
+
+        
     }
 }
