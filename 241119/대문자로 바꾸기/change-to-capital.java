db4fdb9ffada825884151f0ebbedb55584c6d8e2
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-11-19 14:54:03 +0900

    Title: 대문자로 바꾸기, Time: 115ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241119/대문자로 바꾸기/change-to-capital.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String [][] arr2d = new String [5][3];
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
+                arr2d[i][j] = sc.next();
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(arr2d[i][j].toUpperCase() + " ");
+            }
+            System.out.println();
+        }
+    }
+}
