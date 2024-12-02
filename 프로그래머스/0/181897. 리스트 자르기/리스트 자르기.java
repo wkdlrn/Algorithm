b81e6f43cb4423d70d677179a838429ecf66c86d
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-12-02 12:15:10 +0900

    [level 0] Title: 리스트 자르기, Time: 0.04 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Solution {
+    public List solution(int n, int[] slicer, int[] num_list) {
+        List<Integer> answer = new ArrayList<>();
+        
+            if (n == 1) {
+                for(int i = 0; i <= slicer[1]; i++) {
+                    answer.add(num_list[i]);
+                   
+                }
+                 return answer;
+            }
+            else if (n == 2) {
+                for(int i = slicer[0]; i < num_list.length; i++) {
+                     answer.add(num_list[i]);
+                }
+                return answer;
+            } else if (n == 3) {
+                for(int i = slicer[0]; i <= slicer[1]; i++) {
+                    answer.add(num_list[i]);
+                }
+                 return answer;
+            } else {
+                for (int i = slicer[0]; i <= slicer[1]; i+=slicer[2]) {
+                    answer.add(num_list[i]);
+                }
+                 return answer;
+            }
+        
+    }
+}
