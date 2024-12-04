cb475e7faaeaba1e60978b3d9dcf96403ea01fc9
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-12-04 20:31:34 +0900

    [level 0] Title: 5명씩, Time: 0.03 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] names) {
+        ArrayList <String> list = new ArrayList<>();
+        for (int i = 0 ; i < names.length; i+=5) {
+            list.add(names[i]);
+        }
+
+        String [] answer = new String [list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
