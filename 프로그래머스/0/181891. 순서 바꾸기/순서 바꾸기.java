ce56df29c56cc446dc2c1d382fe630aa7acfd071
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-12-04 19:41:28 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.03 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,11 +1,17 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        int [] num_list1 = Arrays.copyOfRange(num_list, n, num_list.length);
-        int [] num_list2 = Arrays.copyOfRange(num_list, 0, n);
-        int [] answer = new int [num_list1.length + num_list2.length];
-        System.arraycopy(num_list1, 0, answer,0,num_list1.length);
-        System.arraycopy(num_list2, 0, answer, num_list1.length, num_list2.length);
+        int [] answer = new int [num_list.length];
+        int idx = 0;
+        
+        for (int i = n; i < num_list.length; i++) {
+            answer[idx] = num_list[i];
+            idx++;
+        }
+        for (int i = 0 ; i < n; i++) {
+            answer[idx]  = num_list[i];
+            idx++;
+        }
         return answer;
     }
 }
