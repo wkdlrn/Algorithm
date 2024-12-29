392d7de43b912a22f87cf6922c778a520b657af5
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2024-12-29 17:07:38 +0900

    [level 0] Title: 배열 만들기 6, Time: 38.24 ms, Memory: 137 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr) {
+        
+        ArrayList<Integer> stkList = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if(stkList.isEmpty()) {
+                stkList.add(arr[i]);
+            } else {
+                if(stkList.get(stkList.size() -1).equals(arr[i])) {
+                    stkList.remove(stkList.size()-1);
+                } else {
+                    stkList.add(arr[i]);
+                }
+            }
+        }
+        if (stkList.isEmpty()) {
+            return new int [] {-1};
+        }
+        int [] answer = new int [stkList.size()];
+        for(int i = 0; i < stkList.size(); i++) {
+            answer[i] = stkList.get(i);
+        }
+        
+        return answer;
+    }
+}
