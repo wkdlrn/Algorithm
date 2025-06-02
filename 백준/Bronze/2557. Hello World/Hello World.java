02c401bb77c9f55cd24526360180d165f8f28d50
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2025-06-02 16:56:55 +0900

    [Bronze V] Title: Hello World, Time: 92 ms, Memory: 13860 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void solution() {
+        System.out.println("Hello World!");
+    }
+    
+    public static void main(String[] args) {
+        solution();
+    }
+}
