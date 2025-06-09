22a9078a4ec37cf03c1621cdfbb5b2c8f48424d2
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2025-06-09 21:18:56 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.03 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] date1, int[] date2) {
+        for(int i=0;i<date1.length;i++) {
+            if(date1[i] < date2[i]) {
+                return 1;
+            }
+            if (date1[i] > date2[i]) {
+                return 0;
+            }        
+        }
+        return 0;
+        
+
+    }
+}
