08cdd727f9f883f5406e6e186a50ee6d25485d66
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2025-09-03 19:48:09 +0900

    [Bronze V] Title: A+B - 7, Time: 132 ms, Memory: 16044 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            System.out.println("Case #" + (i + 1) + ": " + (A + B));
+            
+        }
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
