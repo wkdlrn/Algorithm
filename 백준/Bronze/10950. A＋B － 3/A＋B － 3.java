b8ef39c5cf28831e9bf52be3b6841364057f96d5
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2025-09-03 10:15:33 +0900

    [Bronze V] Title: A+B - 3, Time: 108 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            System.out.println(A + B);
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
