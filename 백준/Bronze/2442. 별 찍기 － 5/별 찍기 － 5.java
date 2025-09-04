17dbea50401909a66e199ae965f83bc3736c9ae9
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2025-09-04 23:32:43 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 292 ms, Memory: 17036 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < N-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2*i-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
