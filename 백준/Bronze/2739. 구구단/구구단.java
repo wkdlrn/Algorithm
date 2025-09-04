20db0fe14261ae73723c7797792116bae0fe8610
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2025-09-04 22:11:21 +0900

    [Bronze V] Title: 구구단, Time: 128 ms, Memory: 16148 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 1; i <= 9; i++) {
+            System.out.println(N + " * " + i + " = " + (N*i));
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
