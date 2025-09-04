a458d40502ec72708f558c72750418864505265c
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2025-09-04 21:33:38 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while((line = br.readLine())!= null) {
+            System.out.println(line);
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
