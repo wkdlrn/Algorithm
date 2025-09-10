b4dbd47da81c046202a50de5b9b1a9dc07f3ca2a
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2025-09-10 17:57:57 +0900

    [Bronze II] Title: 농구 경기, Time: 100 ms, Memory: 14072 KB -BaekjoonHub

---
+++ 백준/Bronze/1159. 농구 경기/농구 경기.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] cnt = new int [26];
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 0; i < N; i++) {
+            String s = br.readLine();
+            cnt[s.charAt(0) - 'a']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < 26; i++) {
+            if(cnt[i] >= 5) {
+                sb.append((char)(i + 'a'));
+            }
+        }
+        if (sb.length() == 0) {
+            System.out.println("PREDAJA");
+        } else {
+            System.out.println(sb.toString());
+        }
+    }
+}
+
+
+
+
+
