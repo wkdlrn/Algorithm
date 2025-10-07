2b8d548682d8e570d66e23fde11d298d7551df4d
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2025-10-07 20:15:41 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 100 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -6,27 +6,29 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int t = Integer.parseInt(br.readLine()); // 테스트케이스 개수
+        int t = Integer.parseInt(br.readLine());
         while (t-- > 0) {
-            int n = Integer.parseInt(br.readLine()); // 의상 수
+            int n = Integer.parseInt(br.readLine());
             Map<String, Integer> map = new HashMap<>();
 
             for (int i = 0; i < n; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
-                String a = st.nextToken(); // 의상 이름 (사용 안함)
-                String b = st.nextToken(); // 의상 종류
-                map.put(b, map.getOrDefault(b, 0) + 1);
+                String itemName = st.nextToken();
+                String category = st.nextToken();
+                if (map.containsKey(category)) {
+                    map.put(category, map.get(category) + 1);
+                } else {
+                    map.put(category, 1);
+                }
             }
-
             long ret = 1;
-            for (int count : map.values()) {
-                ret *= (count + 1); // 해당 종류를 안 입는 경우까지 포함
+            for(int count : map.values()) {
+                ret = ret * (count + 1);
             }
-            ret--; // 전부 안 입는 경우 제외
-
+            ret--;
             sb.append(ret).append("\n");
         }
-
-        System.out.print(sb.toString());
+        System.out.println(sb.toString());
     }
+
 }
