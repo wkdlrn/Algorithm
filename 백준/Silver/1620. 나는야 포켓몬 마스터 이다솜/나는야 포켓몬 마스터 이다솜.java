f904a86db62c9175563de36354aecbd1fed6054e
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2025-10-07 19:38:18 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 444 ms, Memory: 50440 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -4,30 +4,34 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken()); // 포켓몬 개수
-        int m = Integer.parseInt(st.nextToken()); // 문제 개수
-
-        Map<String, Integer> map = new HashMap<>();
-        String[] pokemon = new String[n + 1];
+        
+        StringBuilder sb = new StringBuilder();
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        Map<String, Integer> nameToNum = new HashMap<>();
+        String [] pokemon = new String [n + 1];
 
         for(int i = 1; i <= n; i++) {
             String input = br.readLine();
-            map.put(input,i);
+            nameToNum.put(input, i);
             pokemon[i] = input;
         }
 
         for (int i = 0; i < m; i++) {
             String q = br.readLine();
-            if(q.matches("[0-9]+")) {
+            if(Character.isDigit(q.charAt(0))) {
                 int idx = Integer.parseInt(q);
                 sb.append(pokemon[idx]).append("\n");
             } else {
-                sb.append(map.get(q)).append("\n");
+                sb.append(nameToNum.get(q)).append("\n");
             }
         }
         System.out.println(sb.toString());
+
+
     }
+
 }
