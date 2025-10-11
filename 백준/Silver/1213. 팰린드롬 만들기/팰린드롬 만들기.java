e176856a36d796cfe0528d9e212aefce8d7060f2
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2025-10-11 22:51:40 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -1,40 +1,42 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
 
         int[] cnt = new int[200];
-        for (char c : s.toCharArray()) cnt[c]++;
+        for(char c : s.toCharArray()) {
+            cnt[c]++;
+        }
 
-        StringBuilder ret = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         int flag = 0;
         char mid = 0;
 
-        for (int i = 'Z'; i >= 'A'; i--) {
-            if (cnt[i] > 0) {
-                if ( (cnt[i] & 1) == 1 ) { // cnt[i]가 홀수라면
+        for(int i = 'Z'; i >= 'A'; i--) {
+            if(cnt[i] > 0) {
+                if ( (cnt[i] & 1) == 1 ) {
                     mid = (char)i;
                     flag++;
                     cnt[i]--;
                 }
-                if (flag == 2) break;
-                for (int j = 0; j < cnt[i]; j += 2) {
-                    ret.insert(0, (char)i);
-                    ret.append((char)i);
+                if(flag == 2) break;
+                for(int j = 0; j < cnt[i]; j += 2) {
+                    sb.insert(0,(char)i);
+                    sb.append((char)i);
                 }
-
             }
         }
 
-        if (mid != 0) ret.insert(ret.length() / 2, mid);
-
+        if (mid != 0) {
+            sb.insert(sb.length() / 2, mid);
+        }
         if (flag == 2) {
             System.out.println("I'm Sorry Hansoo");
         } else {
-            System.out.println(ret.toString());
+            System.out.println(sb.toString());
         }
     }
+
 }
