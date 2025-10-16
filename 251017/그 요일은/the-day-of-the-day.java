aacf7b7cb1dcf1c56aa802f62ad8f419e2ab6c49
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2025-10-17 00:34:53 +0900

    Title: 그 요일은, Time: 125ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251017/그 요일은/the-day-of-the-day.java
+++ 251017/그 요일은/the-day-of-the-day.java
@@ -8,7 +8,7 @@ public class Main {
         int m2 = sc.nextInt(); // 목표 날짜 월
         int d2 = sc.nextInt(); // 목표 날짜 일
 
-        int[] months = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int[] months = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
       
         int start = d1;
         for (int i = 1; i < m1; i++) start += months[i];
@@ -16,7 +16,7 @@ public class Main {
         int end = d2;
         for (int i = 1; i < m2; i++) end += months[i];
         
-        int diff = Math.abs(end - start);
+        int diff = end - start;
         int ret = diff/7 + 1;
         System.out.println(ret);
         
