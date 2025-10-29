23e8eeaef45becf75255e5172100411b96c0a15b
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2025-10-29 23:20:29 +0900

    [Silver IV] Title: 좋은 단어, Time: 224 ms, Memory: 24136 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.java
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -1,29 +1,31 @@
 import java.io.*;
-import java.util.*;
+import java.util.ArrayDeque;
 
 public class Main {
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine()); // 단어 개수
-        int cnt= 0;
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
 
-        for (int i = 0; i < n; i++) {
-            String s = br.readLine();
-            Deque<Character> dq = new ArrayDeque<>();
+        for(int i = 0; i < n; i++) {
+            String input = br.readLine();
 
-            for (char c : s.toCharArray()) {
-                if (!dq.isEmpty() && dq.peekFirst() == c) {
-                    dq.pollFirst(); // 짝이 맞으면 제거
+            ArrayDeque<Character> dq = new ArrayDeque<>();
+            for(char c : input.toCharArray()) {
+
+                if(!dq.isEmpty() && c == dq.peekFirst()) {
+                    dq.pollFirst();
                 } else {
-                    dq.addFirst(c); // 아니면 추가
+                    dq.addFirst(c);
                 }
             }
-
-            if (dq.isEmpty()) {
-                cnt++; // 스택이 비어있으면 좋은 단어
+            if(dq.isEmpty()) {
+                cnt++;
             }
         }
-
         System.out.println(cnt);
     }
 }
+
