f5598d469b3b27596b375c35743e87dd18bb3d3f
wkdlrn <135536204+wkdlrn@users.noreply.github.com>
2025-12-03 23:13:43 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 120 ms, Memory: 15864 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -2,17 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
 
-        int[] arr = new int [26];
-        for(int i = 0; i < input.length(); i++) {
-            arr[input.charAt(i) - 'a']++;
-        }
-        for(int ret : arr) {
-            System.out.print(ret + " ");
-        }
+    public static void main(String[] args)throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       String input = br.readLine();
+       int[] a = new int [26];
+
+       for(int i = 0; i < input.length(); i++) {
+           a[input.charAt(i) - 'a']++;
+       }
+       for(int ret : a) {
+           System.out.print(ret + " ");
+       }
     }
-
 }
+
